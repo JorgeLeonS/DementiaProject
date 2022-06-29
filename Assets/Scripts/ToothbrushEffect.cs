2b5d05d7fb69eba9c4633ee2f09d76243fd03aa6
elaine2700 <easerrano.e@gmail.com>
2022-06-28 20:26:18 -0700

    Toothbrush Materials and Sequence

--- Assets/Scripts/ToothbrushEffect.cs
+++ Assets/Scripts/ToothbrushEffect.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ToothbrushEffect : MonoBehaviour
 {
     [SerializeField] List<float> timers = new List<float>();
+    [SerializeField] List<string> prompts = new List<string>();
     [SerializeField] GameObject helpButton;
+    [SerializeField] GameObject textSign;
+    [SerializeField] Transform toothbrush;
 
+    List<Material> toothbrushMats = new List<Material>();
     float timer;
     int indexSequence = 0;
+    float toothbrushtimer = 0f;
 
     private void OnEnable()
     {
@@ -22,34 +28,79 @@ public class ToothbrushEffect : MonoBehaviour
 
     private void Start()
     {
-        helpButton.gameObject.SetActive(false);
         timer = timers[indexSequence];
+        helpButton.gameObject.SetActive(false);
+        SetSequence();
+        textSign.GetComponentInChildren<TextMeshProUGUI>().text = prompts[indexSequence];
+        textSign.SetActive(true);
+        foreach (Material material in toothbrush.GetComponent<Renderer>().materials)
+        {
+            toothbrushMats.Add(material);
+        }
     }
 
     private void Update()
     {
         timer -= Time.deltaTime;
-        if (timer > 0)
+        if (timer < 0)
         {
-            Debug.Log($"time left: {timer}");
+            if (indexSequence < timers.Count - 1)
+            {
+                helpButton.gameObject.SetActive(true);
+                textSign.SetActive(false);
+            }
         }
         else
         {
-            // if index is last dont reactivate.
-            helpButton.gameObject.SetActive(true);
+            Debug.Log($"time left: {timer}");
+        }
+        if(indexSequence == 1)
+        {
+            toothbrushtimer += Time.deltaTime;
+            if(toothbrushtimer < 5f)
+            {
+                foreach (Material material in toothbrushMats)
+                {
+                    material.SetFloat("_Power", toothbrushtimer * 0.1f);
+                }
+            }
         }
     }
 
     private void NextSequence()
     {
         indexSequence++;
-        helpButton.gameObject.SetActive(false);
+        SetSequence();
         if (indexSequence < timers.Count)
         {
+            helpButton.gameObject.SetActive(false);
+            textSign.GetComponentInChildren<TextMeshProUGUI>().text = prompts[indexSequence];
+            textSign.SetActive(true);
             timer = timers[indexSequence];
         }
     }
 
-    // todo text "help is coming, but keep trying to find the toothbrush"
+    private void SetSequence()
+    {
+        switch (indexSequence)
+        {
+            case 0:
+                toothbrush.gameObject.SetActive(false);
+                break;
+            case 1:
+                toothbrush.gameObject.SetActive(true);// activate refraction effect
+                // set tootbrush active
+                break;
+            case 2:
+                // wait for james
+                // fade out and fade in
+                // change to pill sequence
+                break;
+            default:
+                Debug.Log("Out of range");
+                break;
+        }
+    }
+
     // todo make toothbrush to start to appear but difficult to grab
 }
